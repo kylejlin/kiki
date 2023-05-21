d95a83a527a7b5b3afd5e6f77eb4ecbc5d2754d3
Kyle Lin <kylejlin.us@gmail.com>
2023-05-20 23:21:15 -0700

    Parse OptItems

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,37 @@ lalrpop_mod!(pub calc1); // synthesized by LALRPOP
 
 #[test]
 fn calc1() {
-    assert_eq!(Ok(0), calc1::TermParser::new().parse("_"));
-    assert_eq!(Ok(1), calc1::TermParser::new().parse("foo"));
-    assert_eq!(Ok(2), calc1::TermParser::new().parse("(((($foo2))))"));
+    assert_eq!(0, calc1::FileParser::new().parse(" ").unwrap().items.len());
+    assert_eq!(
+        1,
+        calc1::FileParser::new()
+            .parse("[item]")
+            .unwrap()
+            .items
+            .len()
+    );
+    assert_eq!(
+        2,
+        calc1::FileParser::new()
+            .parse("[item] [item]")
+            .unwrap()
+            .items
+            .len()
+    );
 
-    assert!(calc1::TermParser::new().parse("2").is_err());
+    assert!(calc1::FileParser::new().parse("foo").is_err());
+}
+
+#[cfg(test)]
+mod impl_cst {
+    use crate::cst::*;
+
+    impl OptItems {
+        pub fn len(&self) -> usize {
+            match self {
+                OptItems::Nil => 0,
+                OptItems::Cons(tail, _) => 1 + tail.len(),
+            }
+        }
+    }
 }
