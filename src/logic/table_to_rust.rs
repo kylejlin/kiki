2ce71a787c346328d364ed7fb527ed268b7e12b5
Kyle Lin <kylejlin.us@gmail.com>
2023-05-26 16:40:33 -0700

    Finish Nonterminal::Struct case of nonterminal_type_defs

--- src/logic/table_to_rust.rs
+++ src/logic/table_to_rust.rs
@@ -252,7 +252,26 @@ pub fn table_to_rust(table: &Table, file: ValidatedFile) -> Result<RustSrc, Kiki
                 let nonterminal_name = &s.name.name;
                 let fieldset = match &s.fieldset {
                     Fieldset::Empty => ";".to_owned(),
-                    Fieldset::Named(fieldset) => todo!(),
+                    Fieldset::Named(fieldset) => {
+                        let fields_indent_1 = fieldset.fields
+                            .iter()
+                            .filter_map(|field| match (&field.name, &field.symbol) {
+                                (IdentOrUnderscore::Underscore, _) => None,
+                                (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Ident(field_type)) => {
+                                    let field_name = &field_name.name;
+                                    let field_type_name = &field_type.name;
+                                    Some(format!("{field_name}: {field_type_name},"))
+                                }
+                                (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Terminal(field_type)) => {
+                                    let field_name = &field_name.name;
+                                    let field_type_name = file.terminal_enum.get_type(&field_type.name).unwrap();
+                                    Some(format!("{field_name}: {field_type_name},"))
+                                }
+                            })
+                            .collect::<Vec<_>>().join("\n")
+                            .indent(1);
+                        format!("{{\n{fields_indent_1}\n}}")
+                    },
                     Fieldset::Tuple(fieldset) => {
                         let fields_indent_1 = fieldset.fields
                             .iter()
@@ -271,7 +290,7 @@ pub fn table_to_rust(table: &Table, file: ValidatedFile) -> Result<RustSrc, Kiki
                             .indent(1);
                         format!("(\n{fields_indent_1}\n);")
                     }
-                }
+                };
                 format!("{NONTERMINAL_DERIVE_CLAUSE}\npub struct {nonterminal_name}{fieldset}")
             },
             Nonterminal::Enum(e) => todo!(),
