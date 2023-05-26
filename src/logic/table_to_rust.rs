f17ef8b1794faf876b23a5aabfb062c7af0220db
Kyle Lin <kylejlin.us@gmail.com>
2023-05-26 16:51:39 -0700

    Add get_fieldset_src

--- src/logic/table_to_rust.rs
+++ src/logic/table_to_rust.rs
@@ -250,47 +250,7 @@ pub fn table_to_rust(table: &Table, file: ValidatedFile) -> Result<RustSrc, Kiki
         .map(|nonterminal| match nonterminal {
             Nonterminal::Struct(s) => {
                 let nonterminal_name = &s.name.name;
-                let fieldset = match &s.fieldset {
-                    Fieldset::Empty => ";".to_owned(),
-                    Fieldset::Named(fieldset) => {
-                        let fields_indent_1 = fieldset.fields
-                            .iter()
-                            .filter_map(|field| match (&field.name, &field.symbol) {
-                                (IdentOrUnderscore::Underscore, _) => None,
-                                (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Ident(field_type)) => {
-                                    let field_name = &field_name.name;
-                                    let field_type_name = &field_type.name;
-                                    Some(format!("{field_name}: {field_type_name},"))
-                                }
-                                (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Terminal(field_type)) => {
-                                    let field_name = &field_name.name;
-                                    let field_type_name = file.terminal_enum.get_type(&field_type.name).unwrap();
-                                    Some(format!("{field_name}: {field_type_name},"))
-                                }
-                            })
-                            .collect::<Vec<_>>().join("\n")
-                            .indent(1);
-                        format!("{{\n{fields_indent_1}\n}}")
-                    },
-                    Fieldset::Tuple(fieldset) => {
-                        let fields_indent_1 = fieldset.fields
-                            .iter()
-                            .filter_map(|field| match field {
-                                TupleField::Skipped(_) => None,
-                                TupleField::Used(IdentOrTerminalIdent::Ident(field_type)) => {
-                                    let field_type_name = &field_type.name;
-                                    Some(format!("{field_type_name},"))
-                                }
-                                TupleField::Used(IdentOrTerminalIdent::Terminal(field_type)) => {
-                                    let field_type_name = file.terminal_enum.get_type(&field_type.name).unwrap();
-                                    Some(format!("{field_type_name},"))
-                                }
-                            })
-                            .collect::<Vec<_>>().join("\n")
-                            .indent(1);
-                        format!("(\n{fields_indent_1}\n);")
-                    }
-                };
+                let fieldset = get_fieldset_src(&s.fieldset, &file.terminal_enum);
                 format!("{NONTERMINAL_DERIVE_CLAUSE}\npub struct {nonterminal_name}{fieldset}")
             },
             Nonterminal::Enum(e) => todo!(),
@@ -450,6 +410,50 @@ fn create_unique_identifier(preferred_name: &str, used: &mut HashSet<String>) ->
             return name;
         }
         i += 1;
+    }
+}
+
+fn get_fieldset_src(fieldset: &Fieldset, terminal_enum: &TerminalEnum) -> String {
+    match fieldset {
+        Fieldset::Empty => ";".to_owned(),
+        Fieldset::Named(fieldset) => {
+            let fields_indent_1 = fieldset.fields
+                .iter()
+                .filter_map(|field| match (&field.name, &field.symbol) {
+                    (IdentOrUnderscore::Underscore, _) => None,
+                    (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Ident(field_type)) => {
+                        let field_name = &field_name.name;
+                        let field_type_name = &field_type.name;
+                        Some(format!("{field_name}: {field_type_name},"))
+                    }
+                    (IdentOrUnderscore::Ident(field_name), IdentOrTerminalIdent::Terminal(field_type)) => {
+                        let field_name = &field_name.name;
+                        let field_type_name = terminal_enum.get_type(&field_type.name).unwrap();
+                        Some(format!("{field_name}: {field_type_name},"))
+                    }
+                })
+                .collect::<Vec<_>>().join("\n")
+                .indent(1);
+            format!("{{\n{fields_indent_1}\n}}")
+        },
+        Fieldset::Tuple(fieldset) => {
+            let fields_indent_1 = fieldset.fields
+                .iter()
+                .filter_map(|field| match field {
+                    TupleField::Skipped(_) => None,
+                    TupleField::Used(IdentOrTerminalIdent::Ident(field_type)) => {
+                        let field_type_name = &field_type.name;
+                        Some(format!("{field_type_name},"))
+                    }
+                    TupleField::Used(IdentOrTerminalIdent::Terminal(field_type)) => {
+                        let field_type_name = terminal_enum.get_type(&field_type.name).unwrap();
+                        Some(format!("{field_type_name},"))
+                    }
+                })
+                .collect::<Vec<_>>().join("\n")
+                .indent(1);
+            format!("(\n{fields_indent_1}\n);")
+        }
     }
 }
 
