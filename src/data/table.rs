c5f3337edeb147137cf7df9bfd3ebdba73e0015f
Kyle Lin <kylejlin.us@gmail.com>
2023-05-27 14:19:27 -0700

    Add Table::{set_action, set_goto}

--- src/data/table.rs
+++ src/data/table.rs
@@ -29,6 +29,20 @@ impl Table {
     /// 1. Panics if the terminal is not in the table.
     /// 2. Panics if the state is too large.
     pub fn action(&self, state: usize, terminal: &str) -> Action {
+        *self.action_mut(state, terminal)
+    }
+
+    /// ## Panics
+    /// 1. Panics if the terminal is not in the table.
+    /// 2. Panics if the state is too large.
+    pub fn set_action(&self, state: usize, terminal: &str, val: Action) {
+        *self.action_mut(state, terminal) = val;
+    }
+
+    /// ## Panics
+    /// 1. Panics if the terminal is not in the table.
+    /// 2. Panics if the state is too large.
+    fn action_mut(&self, state: usize, terminal: &str) -> &mut Action {
         let terminal_index = self
             .terminals
             .iter()
@@ -40,17 +54,31 @@ impl Table {
             panic!("State {state} is too large. There are only {states} states.");
         }
 
-        self.actions[state * self.terminals.len() + terminal_index]
+        &mut self.actions[state * self.terminals.len() + terminal_index]
     }
 
     /// ## Panics
     /// 1. Panics if the nonterminal is not in the table.
     /// 2. Panics if the state is too large.
-    pub fn goto(&self, state: usize, nontermial: &str) -> Goto {
+    pub fn goto(&self, state: usize, nonterminal: &str) -> Goto {
+        *self.goto_mut(state, nonterminal)
+    }
+
+    /// ## Panics
+    /// 1. Panics if the nonterminal is not in the table.
+    /// 2. Panics if the state is too large.
+    pub fn set_goto(&self, state: usize, nonterminal: &str, val: Goto) {
+        *self.goto_mut(state, nonterminal) = val;
+    }
+
+    /// ## Panics
+    /// 1. Panics if the nonterminal is not in the table.
+    /// 2. Panics if the state is too large.
+    fn goto_mut(&self, state: usize, nonterminal: &str) -> &mut Goto {
         let nonterminal_index = self
             .nonterminals
             .iter()
-            .position(|t| t == nontermial)
+            .position(|t| t == nonterminal)
             .expect("Nonterminal not found in table");
 
         if state >= self.states() {
@@ -58,6 +86,6 @@ impl Table {
             panic!("State {state} is too large. There are only {states} states.");
         }
 
-        self.gotos[state * self.nonterminals.len() + nonterminal_index]
+        &mut self.gotos[state * self.nonterminals.len() + nonterminal_index]
     }
 }
