0509c823fbc9e2413491a536fd07e829bf442860
Kyle Lin <kylejlin.us@gmail.com>
2023-05-27 23:15:39 -0700

    Add crate::tests::balanced_parens

--- src/tests.rs
+++ src/tests.rs
@@ -15,3 +15,11 @@ fn kiki() {
         .parse(src)
         .expect("should parse correctly");
 }
+
+#[test]
+fn balanced_parens() {
+    let src = include_str!("examples/balanced_parens.kiki");
+    parser::FileParser::new()
+        .parse(src)
+        .expect("should parse correctly");
+}
