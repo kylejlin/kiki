f1f9d6294742b0a25131eaddd012868a518e1431
Kyle Lin <kylejlin.us@gmail.com>
2023-05-31 00:41:28 -0700

    Add RuleIndex

--- src/data/machine.rs
+++ src/data/machine.rs
@@ -14,12 +14,17 @@ pub struct State {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Item {
-    pub left: String,
-    pub right: Vec<String>,
+    pub rule: RuleIndex,
     pub lookahead: String,
     /// The `dot` is the index of the symbol to the right of the dot.
     /// If the dot is at the end of the RHS, then `dot == right.len()`.
     pub dot: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum RuleIndex {
+    Original(usize),
+    Augmented,
 }
 
 #[derive(Debug, Clone)]
