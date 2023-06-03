bf29218525b70f08bcfe69a48ff88827747dfaba
Kyle Lin <kylejlin.us@gmail.com>
2023-06-02 17:55:43 -0700

    Outline add_actions_to_table

--- src/pipeline/machine_to_table.rs
+++ src/pipeline/machine_to_table.rs
@@ -2,8 +2,9 @@ use crate::data::{machine::*, table::*, validated_file::*, KikiErr, *};
 
 pub fn machine_to_table(machine: &Machine, file: &File) -> Result<Table, KikiErr> {
     let mut table = get_empty_table(machine, file);
-    add_actions_to_table(&mut table, machine)?;
-    add_gotos_to_table(&mut table, machine)?;
+    let rules: Vec<Rule> = file.get_rules().collect();
+    add_actions_to_table(&mut table, machine, &rules)?;
+    add_gotos_to_table(&mut table, machine, &rules)?;
     Ok(table)
 }
 
@@ -46,10 +47,45 @@ fn get_empty_goto_table(states: &[State], nonterminals: &[String]) -> Vec<Goto> 
     vec![Goto::Err; size]
 }
 
-fn add_actions_to_table(table: &mut Table, machine: &Machine) -> Result<(), KikiErr> {
+fn add_actions_to_table(
+    table: &mut Table,
+    machine: &Machine,
+    rules: &[Rule],
+) -> Result<(), KikiErr> {
+    for (state_index, state) in machine
+        .states
+        .iter()
+        .enumerate()
+        .map(|(i, state)| (StateIndex(i), state))
+    {
+        add_state_actions_to_table(table, state_index, state, &machine.transitions, rules)?;
+    }
+    Ok(())
+}
+
+fn add_state_actions_to_table(
+    table: &mut Table,
+    state_index: StateIndex,
+    state: &State,
+    transitions: &Oset<Transition>,
+    rules: &[Rule],
+) -> Result<(), KikiErr> {
+    for item in &state.items {
+        add_item_actions_to_table(table, state_index, item, transitions, rules)?;
+    }
+    Ok(())
+}
+
+fn add_item_actions_to_table(
+    table: &mut Table,
+    state_index: StateIndex,
+    item: &Item,
+    transitions: &Oset<Transition>,
+    rules: &[Rule],
+) -> Result<(), KikiErr> {
     todo!()
 }
 
-fn add_gotos_to_table(table: &mut Table, machine: &Machine) -> Result<(), KikiErr> {
+fn add_gotos_to_table(table: &mut Table, machine: &Machine, rules: &[Rule]) -> Result<(), KikiErr> {
     todo!()
 }
