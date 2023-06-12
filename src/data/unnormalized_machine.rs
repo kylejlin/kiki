2eeeddb429077964f7d5177f62561275c75a9f48
Kyle Lin <kylejlin.us@gmail.com>
2023-06-11 21:05:12 -0700

    Document UnnormalizedMachine

--- src/data/unnormalized_machine.rs
+++ src/data/unnormalized_machine.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 
 #[derive(Debug, Clone)]
 pub struct UnnormalizedMachine {
+    /// The first state must be the start state.
     pub states: Vec<State>,
     pub transitions: HashSet<Transition>,
 }
