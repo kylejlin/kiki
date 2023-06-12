38f07e066513a9b1e9a45ec6abf2fc8f8e038372
Kyle Lin <kylejlin.us@gmail.com>
2023-06-12 02:17:32 -0700

    Update build.rs to use Kiki
    This breaks the build.
    I will fix it in future commits.

--- build.rs
+++ build.rs
@@ -1,5 +1,38 @@
-extern crate lalrpop;
+extern crate kiki;
+extern crate sha256;
+extern crate walkdir;
+
+use kiki::RustSrcRef;
+use walkdir::WalkDir;
+
+use std::fs;
+use std::path::Path;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    for entry in WalkDir::new("./src") {
+        let entry = entry.unwrap();
+        if entry.path().ends_with(".kiki") {
+            let file_contents = fs::read_to_string(entry.path()).unwrap();
+            let file_hash = sha256::digest(&*file_contents);
+
+            let rs_path = Path::new(entry.path().file_stem().unwrap()).join(".rs");
+            if let Ok(rs_contents) = fs::read_to_string(&rs_path) {
+                let rs_contents = RustSrcRef(&rs_contents);
+                if kiki::get_grammar_hash(rs_contents) == Some(&file_hash) {
+                    // The .kiki file has not changed.
+                    // Therefore, we don't need to regenerate the .rs file.
+                    continue;
+                }
+            }
+
+            let rust_src = match kiki::generate(&file_contents) {
+                Ok(s) => s,
+                Err(err) => {
+                    let file_path = entry.path().display();
+                    panic!("Invalid Kiki file {file_path}. Error: {err:#?}");
+                }
+            };
+            fs::write(rs_path, &rust_src.0).unwrap();
+        }
+    }
 }
