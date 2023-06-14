74ccd0dfa29a355bcb904f39284a69c2b2b0d6da
Kyle Lin <kylejlin.us@gmail.com>
2023-06-14 12:43:00 -0700

    Switch snapshots to strings
    Previously, snapshots contained `RustSrc("foo bar ...")`.
    Now, they contain `"foo bar ..."`.

--- src/tests.rs
+++ src/tests.rs
@@ -6,42 +6,42 @@ mod should_succeed {
     #[test]
     fn json() {
         let src = include_str!("examples/json.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 
     #[test]
     fn kiki() {
         let src = include_str!("examples/kiki.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 
     #[test]
     fn nonempty_unitlike_fieldset() {
         let src = include_str!("examples/nonempty_unitlike_fieldset.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 
     #[test]
     fn balanced_parens() {
         let src = include_str!("examples/balanced_parens.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 
     #[test]
     fn balanced_parens_esoteric() {
         let src = include_str!("examples/balanced_parens_esoteric.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 
     #[test]
     fn balanced_parens_with_comments() {
         let src = include_str!("examples/balanced_parens_with_comments.kiki");
-        let rust_src = generate(src).expect("should generate Rust code");
+        let RustSrc(rust_src) = generate(src).expect("should generate Rust code");
         insta::assert_debug_snapshot!(rust_src);
     }
 }
