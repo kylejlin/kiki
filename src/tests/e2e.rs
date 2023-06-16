2424a4446dee386e8bc25940c49ed6f826459f55
Kyle Lin <kylejlin.us@gmail.com>
2023-06-15 22:22:16 -0700

    Add e2e::balanced_parens_with_comments tests

--- src/tests/e2e.rs
+++ src/tests/e2e.rs
@@ -74,6 +74,82 @@ mod balanced_parens {
     }
 }
 
+mod balanced_parens_with_comments {
+    use crate::examples::balanced_parens_with_comments::{parse, Expr, Token};
+
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn empty() {
+        let actual = parse([]).unwrap();
+        let expected = Expr::Empty;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn balanced_1() {
+        let actual = parse([lparen(), rparen()]).unwrap();
+        let expected = Expr::Wrap(Box::new(Expr::Empty));
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn balanced_2() {
+        let actual = parse([lparen(), lparen(), rparen(), rparen()]).unwrap();
+        let expected = Expr::Wrap(Box::new(Expr::Wrap(Box::new(Expr::Empty))));
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn unexpected_eof() {
+        let actual = parse([lparen()]).unwrap_err();
+        let expected = None;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn unexpected_lparen() {
+        let actual = parse([lparen(), lparen(), rparen(), lparen()]).unwrap_err();
+        let expected = Some(Token::LParen(()));
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn unexpected_rparen() {
+        let actual = parse([rparen(), lparen()]).unwrap_err();
+        let expected = Some(Token::RParen(()));
+        assert_eq!(expected, actual)
+    }
+
+    fn lparen() -> Token {
+        Token::LParen(())
+    }
+
+    fn rparen() -> Token {
+        Token::RParen(())
+    }
+
+    impl PartialEq for Expr {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+                (Expr::Empty, Expr::Empty) => true,
+                (Expr::Wrap(a_inner), Expr::Wrap(b_inner)) => a_inner == b_inner,
+                _ => false,
+            }
+        }
+    }
+
+    impl PartialEq for Token {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+                (Token::LParen(()), Token::LParen(())) => true,
+                (Token::RParen(()), Token::RParen(())) => true,
+                _ => false,
+            }
+        }
+    }
+}
+
 mod balanced_parens_esoteric {
     use crate::examples::balanced_parens_esoteric::{parse, Expr, Token};
 
