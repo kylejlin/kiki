cda9348c18bd1485d2019084e228fcc6a37a1d05
Kyle Lin <kylejlin.us@gmail.com>
2023-06-15 23:08:12 -0700

    Rename `rust_src_snapshot` to `e2e_dry`

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,7 +2,10 @@ use super::*;
 
 mod get_grammar_hash_tests;
 
-mod rust_src_snapshot;
+/// This module conducts end-to-end "dry run" tests.
+/// A dry run is when we generate the parser,
+/// but we don't actually test said generated parser.
+mod e2e_dry;
 
 mod e2e;
 
